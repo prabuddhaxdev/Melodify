5300c5ddef1a51c639751b857a12b5ed2ee022bd
prabuddha610 <prabuddha610@gmail.com>
2025-07-11 10:43:25 +0530

    Fixed typos

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullName: {
-        type:string,
-        required: true,
+      type: String,
+      required: true,
     },
     imageUrl: {
-        type :String,
-        required: true,
+      type: String,
+      required: true,
     },
     clerkId: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-},{ timestamps : true} /* createdAt, updatedAt*/);
+  },
+  { timestamps: true } //  createdAt, updatedAt
+);
 
-export const user = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
