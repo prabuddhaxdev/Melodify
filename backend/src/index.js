d8b2b21158eba8d464980f09c6e80690531617a1
prabuddha610 <prabuddha610@gmail.com>
2025-07-12 17:20:15 +0530

    Add Protect Route Middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express';
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
+
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
--- backend/src/models/song.model.js
+++ backend/src/models/song.model.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
 
-const songSchema = new mongoose.Schema({
-    title:{
-        title:String,
-        required:true,
+const songSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    artist:{
-        title:String,
-        required: true,
+    artist: {
+      type: String,
+      required: true,
     },
     imageUrl: {
-        type:String,
-        required: true,
+      type: String,
+      required: true,
     },
-    audioUrl:{
-        type:String,
-        required: true,
+    audioUrl: {
+      type: String,
+      required: true,
     },
-    duration:{
-        type: Number,
-        required: true,
+    duration: {
+      type: Number,
+      required: true,
     },
-    albumId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Album",
-        required: false,
+    albumId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Album",
+      required: false,
     },
-},{timestamps: true});
+  },
+  { timestamps: true }
+);
 
 export const Song = mongoose.model("Song", songSchema);
