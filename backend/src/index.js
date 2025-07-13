f25fba8506edc825325e0829f49b2449d4a899ae
prabuddha610 <prabuddha610@gmail.com>
2025-07-13 13:06:58 +0530

    Add Admin Routes and Controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express';
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -9,15 +10,26 @@ import statRoutes from "./routes/stat.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import { connectDB } from "./lib/db.js";
+import fileUpload from "express-fileupload";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
 
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath : true,
+    limits: {
+        fileSize: 10* 1024 * 1024 //10MB max file size
+    },
+}))
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -25,6 +37,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal Server Error" : err.message});
+})
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port " + PORT);
